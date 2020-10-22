eaf1a9cc980f03ec6ffa050220855ec46b770411
Baraja Swargiary <43605760+barajasss@users.noreply.github.com>
2020-10-22 21:30:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const nameInput = document.getElementById('name')
+const downloadBtn = document.getElementById('download-btn')
+
+const image = new Image()
+image.src = 'certificate.jpg'
+image.onload = function () {
+	drawImage()
+}
+
+function drawImage() {
+	// ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+	ctx.font = '40px monotype corsiva'
+	ctx.fillStyle = '#29e'
+	ctx.fillText(nameInput.value, 40, 180)
+}
+
+nameInput.addEventListener('input', function () {
+	drawImage()
+})
+
+downloadBtn.addEventListener('click', function () {
+	downloadBtn.href = canvas.toDataURL('image/jpg')
+	downloadBtn.download = 'Certificate - ' + nameInput.value
+})
